24497e9a6bd83b74e2e73b99ffbbd13d31e5ac3e
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2025-12-30 14:36:44 +0000

    notification count update

--- src/modules/notification/notification.route.ts
+++ src/modules/notification/notification.route.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import {
-  getMyNotifications,
-  markNotificationRead,
-  markAllNotificationsRead
+  NotificationController
 } from "./notification.controller";
 import { supabaseAuth } from "../auth/auth.middleware";
 
 const router = Router();
 
-router.get("/", supabaseAuth, getMyNotifications);
-router.patch("/:id/read", supabaseAuth, markNotificationRead);
-router.patch("/read-all", supabaseAuth, markAllNotificationsRead);
+router.get("/", supabaseAuth, NotificationController.getMyNotifications);
+router.get("/count", supabaseAuth, NotificationController.getNotificationCount);
+router.patch("/:id/read", supabaseAuth, NotificationController.markNotificationRead);
+router.patch("/read-all", supabaseAuth, NotificationController.markAllNotificationsRead);
 
 export default router;

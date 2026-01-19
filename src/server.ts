a1bf2737dc7ea1af9511213ebf329eb7597f4708
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2026-01-19 12:25:38 +0000

    schema update

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,5 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
-import authRoutes from "./modules/auth/auth.route";
-import profileRoutes from "./modules/profile/profile.route";
-import postRoutes from "./modules/post/post.route";
-import notificationRoutes from "./modules/notification/notification.route";
-import chatRoutes from "./modules/chat/chat.route";
-import lineageRoutes from "./modules/lineage/lineage.route";
-import kinshipRoutes from "./modules/kinship/kinship.route";
-import commentRoutes from "./modules/comments/comments.route";
-import feedRoutes from "./modules/feed/feed.route";
-import uploadRoutes from "./modules/upload/upload.routes";
-import mediaRoutes from "./modules/media/media.routes";
-import searchRoutes from "./modules/search/search.route";
-import productRoutes from "./modules/product/product.route";
-import friendsRoutes from "./modules/friends/friends.route";
 import { config } from "dotenv";
 import { connectDB } from "./config/prisma";
 import  { rootRouter } from "./routes";
@@ -33,27 +19,5 @@ app.get("/", (req: Request, res: Response) => {
    res.send("Linuty API is running 🥳");
 });
 app.use("/api", rootRouter);
-
-// app.use("/auth", authRoutes);
-// app.use("/feed", feedRoutes);
-// app.use("/profile", profileRoutes);
-// app.use("/post", postRoutes);
-// app.use("/", commentRoutes);
-// app.use("/product", productRoutes);
-// app.use("/friends", friendsRoutes);
-// app.use("/notifications", notificationRoutes);
-// app.use("/conversations", chatRoutes);
-// app.use("/lineages", lineageRoutes);
-// app.use("/kinships", kinshipRoutes);
-// app.use("/media", mediaRoutes);
-
-// // app.use("/discovery", discoveryRoutes);
-// app.use("/search", searchRoutes);
-// app.use("/internal/media", mediaRoutes);
-// app.use("/uploads", uploadRoutes);
-// app.use("/store", uploadRoutes);
-
-
-
 
 export default app;

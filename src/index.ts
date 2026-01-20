a7ab315ea71cb740871994e5c4b4fa57b636d75e
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2026-01-20 09:42:38 +0000

    index update

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,17 @@ import app from "./server";
 async function main() {
   config();
 
-  try {
-    await connectDB();
-    console.log("DB connected");
-  } catch (err) {
-    console.error("DB connect failed", err);
-    process.exit(1);
-  }
-
   const port = Number(process.env.PORT) || 8080;
-  app.listen(port, "0.0.0.0", () => console.log(`Listening on ${port}`));
+  app.listen(port, "0.0.0.0", () => console.log(`Listening on ${port}`, 
+ 
+    connectDB()
+    .then(() => console.log("DB connected"))
+    .catch((err) => {
+      console.error("DB connect failed", err);
+     process.exit(1);
+    })
+  ), 
+);
 }
 
 main();
